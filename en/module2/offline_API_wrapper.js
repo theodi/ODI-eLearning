e38cb366f5a9e18d4048559ee195f60c4ad821f1
Dave Tarrant <davetaz@theodi.org>
2015-08-11 10:02:27 +0100

    Adding new module2 bits

--- en/module2/offline_API_wrapper.js
+++ en/module2/offline_API_wrapper.js
@@ -1,4 +1,5 @@
 var api_url = "http://localhost/~davetaz/backend/";
+var updateInt;
 if (!localStorage.getItem("ODI_id")) {
   	$.get( api_url + "create_id.php", function( data ) {
   		window.localStorage.setItem("ODI_id",data);
@@ -12,6 +13,10 @@ $.getJSON("course/config.json",function(data) {
 });
 
 $(document).ready(function() {
+	var ODI_id = QueryString.id;
+	if (ODI_id != "") {
+		loadRemote(ODI_id);
+	}
 	$.getJSON("course/config.json",function(data) {
 		moduleId = data._moduleId;
 		if (moduleId == "nav"){
@@ -42,6 +47,15 @@ function getModuleId() {
 	return moduleId;
 }
 
+function loadRemote(id) {
+	url = api_url + "/load.php?id=" + id;
+	$.getJSON(url,function(data) {
+		for (key in data) {
+			window.localStorage.setItem(key,data[key]);		
+		}
+	});
+}
+
 function updateRemote() {
     if(window.localStorage!==undefined) {
         send = {};
@@ -51,13 +65,27 @@ function updateRemote() {
            url: api_url + "store.php",         
            data: send,
            success: function(ret) {
-//		console.log("Data stored in cloud");
-	   }
+    		var d = new Date();
+    		window.localStorage.setItem('ODI_lastSave',d.toUTCString());
+		clearUpdateInt();
+	   },
+	   error: function(ret) {
+    		window.localStorage.setItem('ODI_lastSave',"No connection to server (Saving locally only)");
+		setUpdateInt();
+	   }  
         });
-    } else {
-    }
+    } 
 }
 
+function setUpdateInt() {
+	if (!updateInt) {
+		updateInt = setInterval(function() {updateRemote();},5000);
+	}
+}
+
+function clearUpdateInt() {
+	clearInterval(updateInt);
+}
 function getValue(cname) {
     module_id = getModuleId();
     cname = "ODI_" + module_id + "_" + cname;
@@ -73,6 +101,28 @@ function setValue(cname, cvalue) {
     updateRemote();
 }
 
+var QueryString = function () {
+  // This function is anonymous, is executed immediately and 
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  } 
+    return query_string;
+}();
 
 var API = {
 	LMSInitialize: function() {
