f1faa0fa6f4417cda860e57cf052214357c5c3ce
Dave Tarrant <davetaz@theodi.org>
2015-10-13 17:47:38 +0100

    Added Open Refine hook

--- src/extensions/adapt-contrib-resources/js/adapt-contrib-resources.js
+++ src/extensions/adapt-contrib-resources/js/adapt-contrib-resources.js
@@ -24,19 +24,8 @@ define(function(require) {
 		});
 	
 	}
-	
-	Adapt.once('app:dataReady', function() {
 
-		var resources = Adapt.course.get('_resources');
-	
-		// FIXEME: MAKE LEGACY COMPATIBLE BY DETECTING OBJECT OR ARRAY! 
-		if (resources instanceof Array) {
-		} else {
-			var out = [];
-			out.push(resources);
-			resources = out;
-		}
-
+	function displayResources(resources) {
 		for (i=0;i<resources.length;i++) {
 			var courseResources = resources[i];
 			if (courseResources) {
@@ -51,6 +40,63 @@ define(function(require) {
 			}
 
 			setupResources(courseResources, courseResources._resourcesItems,i);		
+		}
+	}
+	
+	Adapt.once('app:dataReady', function() {
+
+		var resources = Adapt.course.get('_resources');
+	
+		// FIXEME: MAKE LEGACY COMPATIBLE BY DETECTING OBJECT OR ARRAY! 
+		if (resources instanceof Array) {
+		} else {
+			var out = [];
+			out.push(resources);
+			resources = out;
+		}
+	
+		var refine = false;
+		var refineResources = {};
+		var refineIndex = 0;
+		for (i=0;i<resources.length;i++) {
+			var courseResources = resources[i];
+			if (courseResources) {
+				var title = courseResources.title;
+				if (title == "Open Refine Servers") {
+					refine = "true";	
+					refineResources = courseResources;
+					refineIndex = i;
+				}
+			}
+		}
+		if (refine) {
+			var lines = [];
+			$.get("http://training.theodi.org/refine.csv",function(data){
+				lines = data.split('\n');
+			})
+			.done(function() {
+				refineResources._resourcesItems = [];
+				for (k=0;k<lines.length;k++) {
+					if (lines[k] !="") {
+						var item = {};
+						item._type = "link";
+						item.description = "Open refine server for use in data cleaning exercise";
+						item._link = lines[k];
+						item.title = "Refine Server " + (k+1);
+						refineResources._resourcesItems.push(item);
+					}
+				}
+				resources[refineIndex] = refineResources;
+				displayResources(resources);
+			})
+			.fail(function() {
+				displayResources(resources);
+			})
+			.always(function() {
+				$('.navigation-drawer-toggle-button').removeClass('display-none');
+			});
+		} else {
+			displayResources(resources);
 		}
 	});
 
